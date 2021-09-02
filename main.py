55545bdb93c9d8994559b4983e9147b0db796899
GUDINI <89667688+gudini-dev@users.noreply.github.com>
2021-09-03 00:52:30 +0300

    v(0.2)

--- main.py
+++ main.py
@@ -9,12 +9,20 @@ from colorama import Fore, Back, Style
 
 class Design:
 	@staticmethod
+	def softName():
+		return("COOKIES PULL REQUESTS")
+
+	@staticmethod
+	def softVersion():
+		return(" (v0.2)")
+
+	@staticmethod
 	def printHeader():
-		system("title COOKIES PULL REQUESTS v0.1")
+		system("title " + Design.softName() + Design.softVersion())
 		os.system("cls")
 		print("\n " + Fore.GREEN + "█████████████████████████████████" + Style.RESET_ALL)
 		print(" " + Fore.GREEN + "██" + Style.RESET_ALL + Fore.WHITE + "                             " + Style.RESET_ALL + Fore.GREEN + "██" + Style.RESET_ALL)
-		print(" " + Fore.GREEN + "██" + Style.RESET_ALL + Fore.WHITE + "    COOKIES PULL REQUESTS    " + Style.RESET_ALL + Fore.GREEN + "██" + Style.RESET_ALL)
+		print(" " + Fore.GREEN + "██" + Style.RESET_ALL + Fore.WHITE + "    " + Design.softName() + "    " + Style.RESET_ALL + Fore.GREEN + "██" + Style.RESET_ALL)
 		print(" " + Fore.GREEN + "██" + Style.RESET_ALL + Fore.WHITE + "          by GUDINI          " + Style.RESET_ALL + Fore.GREEN + "██" + Style.RESET_ALL)
 		print(" " + Fore.GREEN + "██" + Style.RESET_ALL + Fore.WHITE + "                             " + Style.RESET_ALL + Fore.GREEN + "██" + Style.RESET_ALL)
 		print(" " + Fore.GREEN + "█████████" + Style.RESET_ALL + Back.GREEN + Fore.MAGENTA + "t.me/gudini_dev" + Style.RESET_ALL + Fore.GREEN + "█████████\n" + Style.RESET_ALL)
@@ -24,12 +32,12 @@ class CookiesPullRequests:
 		self.count = 0
 
 	def WorkerStats(self):
-		system("title COOKIES PULL REQUESTS (v0.1)  \\  Statistics bar loading ...")
+		system("title " + Design.softName() + Design.softVersion() + "  \\  Statistics bar loading ...")
 		while True:
-			system("title COOKIES PULL REQUESTS (v0.1)  \\  LEFT COUNT: [" + str(self.count_file) + "]")
+			system("title " + Design.softName() + Design.softVersion() + "  \\  LEFT COUNT: [" + str(self.count_file) + "]")
 			time.sleep(0.5)
 			if self.count_file == 0:
-				system("title COOKIES PULL REQUESTS (v0.1)  \\  RESULT COUNT: [" + str(self.count) + "]")
+				system("title " + Design.softName() + Design.softVersion() + "  \\  RESULT COUNT: [" + str(self.count) + "]")
 				sys.exit()
 
 	def print_error(self, text):
@@ -59,34 +67,38 @@ class CookiesPullRequests:
 		if keys_list == "":
 			self.print_error("The line cannot be empty.")
 			return
-		keys_list = {key.strip() for key in keys_list.split(",")}
+		keys_list = list(filter(None, {key.strip() for key in keys_list.split(",")}))
+		if not len(keys_list):
+			self.print_error("The line cannot be empty.")
+			return
 		print(Fore.BLUE + "\n You entered (" + str(len(keys_list)) +") keys:\n" + Style.RESET_ALL)
 		c = 1
 		for key in keys_list:
 			print(Fore.BLUE + "  " + str(c) + ") " + key + Style.RESET_ALL)
 			c += 1
-		print(Fore.WHITE + "\n [INFO] Enter the path to the cookie folder, (ex: Desktop/cookies)" + Style.RESET_ALL)
+		print(Fore.WHITE + "\n [INFO] Enter the path to the cookie folder, (ex: Desktop/cookies/*.txt)" + Style.RESET_ALL)
 		folder_cookies = input(Fore.GREEN + "\n -> "  + Style.RESET_ALL)
 		if not os.path.exists(folder_cookies):
 			self.print_error("This folder does not exist.")
 			return
-		filenames_cookies = os.listdir(folder_cookies)
+		filenames_cookies = [file_name for file_name in os.listdir(folder_cookies) if os.path.isfile(folder_cookies + "/" + file_name)]
 		if not len(filenames_cookies):
-			self.print_error("Cookies folder is empty.")
+			self.print_error("Cookies folder is empty, no format " + folder_cookies + "/*.txt")
 			return
-		print(Fore.BLUE + "\n Found (" + str(len(filenames_cookies)) +") cookies" + Style.RESET_ALL)
+		print(Fore.BLUE + "\n Found (" + str(len(filenames_cookies)) +") cookies file" + Style.RESET_ALL)
 		print(Fore.WHITE + "\n [INFO] Enter the path where to save new cookies, (ex: Desktop/new_cookies)" + Style.RESET_ALL)
 		folder_new = input(Fore.GREEN + "\n -> "  + Style.RESET_ALL)
+		if folder_new == "":
+			self.print_error("The line cannot be empty.")
+			return
 		if not os.path.exists(folder_new):
 			os.mkdir(folder_new)
 		print("\n " + Fore.BLUE + "███████████████████████" + Style.RESET_ALL)
 		print(" " + Fore.BLUE + "██" + Style.RESET_ALL + Fore.WHITE + "     LOADING ...   " + Style.RESET_ALL + Fore.BLUE + "██" + Style.RESET_ALL)
 		print(" " + Fore.BLUE + "███████████████████████" + Style.RESET_ALL)
-		
 		threading.Thread(target = self.WorkerStats).start()
 		self.count_file = len(filenames_cookies)
 		self.parse(keys_list, folder_cookies, filenames_cookies, folder_new)
-
 		print(Fore.GREEN + "\n [FINISH] Result count cookies: " + str(self.count) + Style.RESET_ALL)
 
 if __name__ == "__main__":
